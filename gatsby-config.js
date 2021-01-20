2f76ab8ea5ff72ebbcb44c67782e872db0b8f351
Ndumiso Mguni <Ndumiso29@gmail.com>
2021-01-20 10:27:55 -0500

    icon name change

--- gatsby-config.js
+++ gatsby-config.js
@@ -77,8 +77,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        // edit below
-        icon: `content/assets/favicon.png`,
+        icon: `src/images/icon.png`,
       },
     },
     {
